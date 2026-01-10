2c985ae028c6fb06524e41a3f7906d2b40c75bb0
nut2275 <nutthapon.sala@gmail.com>
2026-01-10 19:37:46 +0700

    add .env

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,12 +1,15 @@
 // /services/api.ts
 import axios from "axios";
 
+// ✅ ใช้ตัวแปรจาก .env
+// ถ้าหาไม่เจอ ให้กันเหนียวไว้ที่ localhost (Optional)
+const baseURL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
+
 const api = axios.create({
-  baseURL: "http://localhost:5000/", // URL backend Node.js
+  baseURL: baseURL, // url ของ API
   headers: {
     "Content-Type": "application/json",
   },
 });
-
 
 export default api;
